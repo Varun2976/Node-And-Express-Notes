118662cb56531005f41090a49973ef3055d416ff
Varun2976 <vsnair2976@gmail.com>
2025-12-22 17:47:48 +0530

    added Rest api

--- Chapter_2/server.js
+++ Chapter_2/server.js
@@ -1,9 +1,23 @@
 const express = require('express')
 const PORT = 5383
 const app = express()
+let data = {
+    name : "varun"
+}
 app.get('/' , (req,res) => {
-    console.log("System endpoint reached." , req.method);
-    res.sendStatus(200); // First response method -> Send Status - > Send the status of the server depending on the status code which in this case is 200
+    
+    res.send('<h1>This is Homepage</h1>') // First response method -> Send Status - > Send the status of the server depending on the status code which in this case is 200
 
-}); // 
+}); 
+app.get('/about' ,(req,res) => {
+    res.send('<h1>This is about page</h1>');
+})
+
+//Type 2 - API Endpoints
+
+app.get('/app/data' , (req,res) => {
+    console.log('This is for the data part , rest api')
+    res.send(data);
+})
+
 app.listen(PORT, () => console.log(`Server has started at ${PORT}`))
