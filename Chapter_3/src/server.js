074028ef1c47208170b5fdac56350f179abe97b4
Varun2976 <vsnair2976@gmail.com>
2025-12-29 21:18:42 +0530

    Added the Website in working condition

--- Chapter_3/src/server.js
+++ Chapter_3/src/server.js
@@ -1,8 +1,18 @@
 import express from 'express'
+import path, {dirname} from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
 const PORT = process.env.PORT || 5200 // Here the 5200 becomes the backup port
 
+const __filename = fileURLToPath(import.meta.url) //get the file path from the URL to the module
+const __dirname = dirname(__filename) // Get the directory name from the file path
+
+app.use(express.static(path.join(__dirname,'../public')))
+
+app.get('/' , (req,res) => {
+    res.sendFile(path.join(__dirname ,'public' ,'index.html'))
+})
 app.listen(PORT ,()=>{
     console.log(`Server started : ${PORT}`)
 })
