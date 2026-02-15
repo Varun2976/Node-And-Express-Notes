93ba7264f3582217e91f83e33c006e939e648be9
Varun2976 <vsnair2976@gmail.com>
2026-02-16 03:26:21 +0530

    Added

--- Chapter_2/server.js
+++ Chapter_2/server.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const PORT = 5383
 const app = express()
-let data = {
-    name : "varun"
-}
+app.use(express.json()); // Middleware , invokes the express , configure our server to invoke json as a request
+let data = ["Varun"]
 app.get('/' , (req,res) => {
     
     res.send('<h1>This is Homepage</h1>') // First response method -> Send Status - > Send the status of the server depending on the status code which in this case is 200
@@ -11,7 +10,7 @@ app.get('/' , (req,res) => {
 }); 
 app.get('/about' ,(req,res) => {
     res.send(`
-        <body style ="background-color : blue ; color : black" >
+        <body style ="background-color : yellow ; color : black" >
             <h1>DATA TIMEEE !!!!</h1>
             <p>${JSON.stringify(data)}</p>
         </body>
@@ -23,11 +22,23 @@ app.get('/about' ,(req,res) => {
 
 //Type 2 - API Endpoints
 
-app.get('/app/data' , (req,res) => {
+app.get('/api/data' , (req,res) => {
     console.log('This is for the data part , rest api')
-    res.send(data);
-    const newEntry = req.body() //request to access data from the body
+    
+    const newEntry = req.body //request to access data from the body
     res.sendStatus(201) // The created status
+})
+
+app.post('/api/data' , (req,res) => {
+    const newEntry = req.body //When one presses sign up button the browser sends a request and that tells the server what to do
+    data.push(newEntry.name) // pushes the name in data
+    res.sendStatus(201)
+})
+
+app.delete('/api/data' , (req,res) => {
+    const newEntry = req.body
+    data.pop()
+    console.log(`Deleted the end value`)
 })
 
 app.listen(PORT, () => console.log(`Server has started at ${PORT}`))
